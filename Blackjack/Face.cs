97dd1b60877c0d590e96e6a8a8a223910417f019
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2023-03-06 16:32:25 -0600

    Add files via upload

---
+++ Blackjack/Face.cs
@@ -0,0 +1,22 @@
+﻿namespace Blackjack_Pawelski
+{
+    /// <summary>
+    /// Represents the different faces on the cards.
+    /// </summary>
+    public enum Face
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        King,
+        Queen
+    }
+}
---
+++ Blackjack/Suit.cs
@@ -0,0 +1,13 @@
+﻿namespace Blackjack_Pawelski
+{
+    /// <summary>
+    /// Represents the different suits on the cards.
+    /// </summary>
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
